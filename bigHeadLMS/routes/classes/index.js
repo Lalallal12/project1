feea053b551e4eba20714801255fa7fa88bb202d
wonmi <kwm7502@gmail.com>
2023-04-05 10:08:19 +0900

    put add

--- bigHeadLMS/routes/classes/index.js
+++ bigHeadLMS/routes/classes/index.js
@@ -1,5 +1,5 @@
 'use strict'
-const { readAll } = require('../../controller/classController')
+
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
@@ -34,6 +34,48 @@ module.exports = async function (fastify, opts) {
       
       client.release()
     }
+  })
+
+
+  fastify.put('/:id', async function (request, reply) {
+
+    let queryStr ="";
+
+    queryStr = "update class set mod_date =now()"
+    
+    if(request.body.name != undefined)
+      queryStr = queryStr + ", name = '"+ request.body.name +"' ";
+    if(request.body.userId != undefined)
+      queryStr = queryStr + ", user_id = '"+ request.body.userId +"' ";
+    if(request.body.categoryId != undefined)
+      queryStr = queryStr + ", category_id = '"+ request.body.categoryId +"' ";
+    
+
+    queryStr = queryStr+"where id='"+request.params.id+"'";
+/*
+    queryStr =    "update class set mod_date =now() " 
+    queryStr =    queryStr + (request.body.name != undefined)? ", name = '"+ request.body.name +"' ":""
+    queryStr =    queryStr + (request.body.userId != undefined)? ", user_id = '"+ request.body.userId +"' ":""
+    queryStr =    queryStr + (request.body.categoryId != undefined)? ", category_id = '"+ request.body.categoryId +"' ":"";
+ */
+    console.log(queryStr)
+    
+    const client = await fastify.pg.connect();
+    try {
+      const result = await client.query(
+        queryStr
+      )
+
+      const { rows } = await client.query(
+        "SELECT * FROM CLASS where id = '"+request.params.id+"'"
+      )
+
+      reply.code(200).send(rows)
+      
+    } finally {
+      client.release()
+    }
+
   })
   
 }
